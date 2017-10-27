e30fdcb3082ab0a6adef8634b270684a406f27b8
Prateek Hegde <prateekhegde12@gmail.com>
2017-10-27 09:31:57 +0530

    initial commit

---
+++ app/src/main/java/com/scryptonite/prateek/habba18_meeting/MainActivity.java
@@ -0,0 +1,39 @@
+package com.scryptonite.prateek.habba18_meeting;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("blah blah"); // enter url
+    }
+
+    private Boolean exit = false;
+    @Override
+    public void onBackPressed() {
+        if (exit) {
+            finish(); // finish activity
+        } else {
+            Toast.makeText(this, "Press Back again to Exit",
+                    Toast.LENGTH_SHORT).show();
+            exit = true;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    exit = false;
+                }
+            }, 2 * 1000);
+
+        }
+
+    }
+}
